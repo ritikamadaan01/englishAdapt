c6dd87e51ea07bba75962c9a4380f459d7f54f8c
ygarg25 <ygarg25@gmail.com>
2022-07-23 12:02:26 +0530

    update routes

--- src/App.js
+++ src/App.js
@@ -2,33 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { Switch, Route, Redirect, Routes, Navigate } from "react-router-dom";
 import Navbar from './Components/navbar';
-// import Home from './Components/home';
+// import Navbar from './Components/navbar_try';
 import Home from './Components/Home/index';
 import Footer from './Components/footer';
 import BusinessEnglish from './Components/business_eng';
 import Plans from './Components/plans';
-import Blogs from './Components/blog';
+// import Blogs from './Components/blog';
 import "./CSS/main.css"
 
 
 function App() {
   return (
-    // <div className="">
-    //   <Navbar />
-    //   <Home />
-    //   <Footer />
-    // </div>
     <div className=''>
       <Navbar />
       <div>
         <Routes>
-          {/* <Route path="/student/:name" component={EditStudent} /> */}
-          {/* <Route path="/allStudent" render={props => <Show {...props} />} /> */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/businessEnglish" element={<BusinessEnglish />} />
-          <Route path="/plans" element={<Plans />} />
-          {/* <Route path="/blogs" element={<Blogs />} /> */}
+          <Route
+            path="/home" exact element={<Home />} />
+          <Route path="/businessEnglish" exact element={<BusinessEnglish />} />
+          <Route path="/plans" exact element={<Plans />} />
           <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="*" element={<Home />} />
+          {/* <Navigate to="/home" /> */}
         </Routes>
       </div>
       <Footer />
